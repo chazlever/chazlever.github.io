cd5a7a7e5e5b6f2a778dfe2568576c7b66e828bb
Chaz Lever <chazlever@gatech.edu>
2012-05-07 00:26:12 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# chazlever.com
+
+## What is it?
+
+This code in this repo drives the personal web page of [Chaz
+Lever](http://www.chazlever.com). You are free to use or modify this code for
+your own site, but if you do, I ask that you please link back to my site.
+
+## Who are you?
+
+My name is [Chaz Lever](http://www.chazlever.com). I'm a graduate student
+pursuing a PhD in computer science with a focus in computer networking and
+security.
+
---
+++ custom.js
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * Author: Chaz Lever
+ * Date: 05/06/2012
+ ******************************************************************************/
+
+// Main entry point for custom code
+$(document).ready(function() {
+   // Hide abstract elements by default
+   var abstract = 'div.abstract';
+   $(abstract).hide();
+   $('<br /><a class="abstract" href="#" title="View/Hide Abstract">[abstract]</a>')
+      .insertBefore(abstract);
+
+   // Handle the hiding/viewing of abstract
+   $('a.abstract').click(function() {
+      abstract = $(this).next();
+      if ($(abstract).is(":visible")) {
+         $(abstract).hide();
+      }
+      else {
+         //alert($(abstract).html());
+         $(abstract).show();
+      }
+      return false;
+   });
+});
