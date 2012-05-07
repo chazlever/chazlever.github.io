27418090ac06fc1dd5bcc1e75655ac111cc0cc2e
Chaz Lever <chazlever@gatech.edu>
2012-05-07 02:51:06 -0400

    Update spacing to look less cramped

--- custom.js
+++ custom.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
    // Hide abstract elements by default
    var abstract = 'div.abstract';
    $(abstract).hide();
-   $('<br /><a class="abstract" href="#" title="View/Hide Abstract">[abstract]</a>')
+   $('<br class="hide"/><a class="abstract" href="#" title="View/Hide Abstract">[abstract]</a>')
       .insertBefore(abstract);
 
    // Handle the hiding/viewing of abstract
